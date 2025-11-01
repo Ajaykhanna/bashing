fc8396bc3f99c98b53cda90057b72734cd2cfc05
Ajay Khanna <akhanna2@ucmerced.edu>
2025-10-31 20:45:30 -0600

    Add parallel execution for file removal in finding script

--- finding.sh
+++ finding.sh
@@ -10,3 +10,11 @@ find frame_{30001..87531}/gs_nm \
 
 # Check if files are available
 find frame_{30001..87531}/gs_nm -type f -name "*.out" -o -name "coords.xyz" -print
+
+# Finding With Parallel Execution
+find frame_{1..87531} -type f \( -name "*.out" -o -name "coords.xyz" \) -print0 | \
+parallel -0 -j 10 '
+  dir=$(dirname "{}")
+  echo "Removing files from: $dir"
+  rm -f "{}"
+'
